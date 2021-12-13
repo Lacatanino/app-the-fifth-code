30ddad1b3f71b15bfbfdfc87d66ee76e7ca886a0
Lacatanino <catalinaninom@gmail.com>
2021-12-13 00:47:37 -0500

    avancesfront

--- ciclo4_frontend/src/components/nav/nav.js
+++ ciclo4_frontend/src/components/nav/nav.js
@@ -15,16 +15,11 @@ const NavBar = ({ pagina }) => {
                 <Nav.Link href="/usuarios" eventKey="/usuarios">USUARIOS</Nav.Link>
             </Nav.Item>
 
-<<<<<<< HEAD
-=======
-        <Nav.Item>
-            <Nav.Link href="/proyectos" eventKey="/proyectos">PROYECTOS</Nav.Link>
-        </Nav.Item>
+            <Nav.Item>
+            <Nav.Link href="/ListarAvances" eventKey="/ListarAvances">AVANCES</Nav.Link>
+            </Nav.Item>
+        
 
-        <Nav.Item>
-            <Nav.Link href="/Listaravances" eventKey="/Listaravances">AVANCES</Nav.Link>
-        </Nav.Item>
->>>>>>> branch-frontcatalina
         </Nav>
 
     );
